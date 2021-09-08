88bea6f5cea92a8ac874b511c7a1b65fee2c29ef
AhmedOmarAL-farouq <ahmed.omar.alfarouq@gmail.com>
2021-09-08 18:13:24 +0200

    Fix teachers images

--- js/main.js
+++ js/main.js
@@ -1,36 +1,36 @@
 const teachers = [
   {
-    img: '../imgs/teacher1.png',
+    img: './imgs/teacher1.png',
     name: 'Albert Noah',
     position: 'Professional UI & UX Designer',
     description: 'He is one of the most professional UI & UX designers in the world. He worked in google',
   },
   {
-    img: '../imgs/teacher2.png',
+    img: './imgs/teacher2.png',
     name: 'Mohamed Omar',
     position: 'Professional Front-End Developer',
     description: 'He is one of the most professional front end developers in the world. He worked in google',
   },
   {
-    img: '../imgs/teacher3.png',
+    img: './imgs/teacher3.png',
     name: 'Sara Esmail',
     position: 'Professional Back-End Developer',
     description: 'He is one of the most professional back end developers in the world. He worked in amazon',
   },
   {
-    img: '../imgs/teacher4.png',
+    img: './imgs/teacher4.png',
     name: 'Jain Noah',
     position: 'Professional Full-Stack Developer',
     description: 'She is one of the most professional full stach developers in the world. He worked in facebook',
   },
   {
-    img: '../imgs/teacher5.png',
+    img: './imgs/teacher5.png',
     name: 'Maria Mosa',
     position: 'Professional Full-Stack Developer',
     description: 'she is one of the most professional full stach developers in the world. He worked in facebook',
   },
   {
-    img: '../imgs/teacher6.png',
+    img: './imgs/teacher6.png',
     name: 'Ahmed Omar',
     position: 'Professional Full-Stack Developer',
     description: 'He is one of the most professional ffull stach developers in the world. He worked in google',
